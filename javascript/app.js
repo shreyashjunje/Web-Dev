e38d3b7b2d5b09dfbbca021f1937635636416952
shreyashjunje <shreyashjunje2003@gmail.com>
2023-08-17 00:34:27 +0530

    practised questions

---
+++ javascript/app.js
@@ -0,0 +1,47 @@
+let a=5;
+let b=6;
+let c=7;
+
+if(a>=b && a>=c){
+    console.log(a," is greater number");
+}else if(b>=a && b>=c){
+    console.log(b," is greater number");
+}else{
+    console.log(c,` is greater number`);
+}
+
+
+// let str = "apple";
+// if((str[0]=='a' || str[0]=='A') && str.length > 5){
+//     console.log("golden string");
+// }else{
+//     console.log("not a golden string");
+// }
+
+// let month=2;
+// switch(month){
+//     case 1: 
+//         console.log("jan","feb","mar");
+//         break;
+//     case 2:
+//         console.log("april","may","june");
+//         break;
+//     case 3:
+//         console.log("july","aug","sep");
+//         break;
+//     case 4 :
+//         console.log("oct","nov","dec");
+//         break;
+
+// }
+
+// let name= prompt("Enter the name: ");
+// let age = prompt("Enter the age: ");
+// alert(`${name} is ${age} years old`)
+
+// let num=40;
+// if(num % 10 == 0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
