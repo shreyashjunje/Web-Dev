e70d7ae53791025ef105d5fed8188b3cfb44d75c
shreyashjunje <shreyashjunje2003@gmail.com>
2024-01-16 23:32:37 +0530

    .

---
+++ REACT/Learning/interactivity/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LikeBtn from "./components/likebtn.jsx"
+import './App.css'
+
+
+function App(){
+
+
+  return (
+    <div>
+       <LikeBtn/>
+    </div>
+  )
+}
+
+export default App
---
+++ REACT/Learning/interactivity/src/components/likebtn.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./likebtn.css"
+
+function LikeBtn(){
+
+    const[isliked,setIsliked]=useState(false);
+
+    let clickhandler=()=>{
+        setIsliked(!isliked);
+    };  
+
+    let likedStyle = {color:'red'};
+
+    return (
+        <div>
+           <p onClick={clickhandler}>
+            {
+                isliked?(<i class="fa-solid fa-heart" style={likedStyle} ></i> )
+                   
+                :( <i class="fa-regular fa-heart" ></i>)
+                   
+
+                
+                
+               
+            }
+          </p>
+        </div>
+    )
+}
+
+export default LikeBtn;
