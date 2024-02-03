3fb45128aacc114bbe7d2778cc96f1439c28365b
shreyashjunje <shreyashjunje2003@gmail.com>
2024-02-03 23:56:12 +0530

    ..

--- REACT/Ecommerse_landing_page/src/components/leftside_div.jsx
+++ REACT/Ecommerse_landing_page/src/components/leftside_div.jsx
@@ -1,17 +1,27 @@
-import "./leftside_div.css"
+import "./leftside_div.css";
 
-function Leftside(){
-    return (
-        <div className="leftside_div">
-            <div className="heading_div">
-                <h2>Find perfect plants</h2>
-                <h2>for your home</h2>
-            </div>
-            <p>Beautiful plants that<br></br> encourage you to get creative</p>
-            <button>SHOP NOW</button>
-        
-        </div>
-    )
+function Leftside() {
+  let clickhandler = () => {
+    console.log("hi");
+  };
+
+  let DoubleClickhandler = () => {
+    alert("bhai ek bar hi press karna hai ");
+  };
+  return (
+    <div className="leftside_div">
+      <div className="heading_div">
+        <h2>Find perfect plants</h2>
+        <h2>for your home</h2>
+      </div>
+      <p>
+        Beautiful plants that<br></br> encourage you to get creative
+      </p>
+      <button onClick={clickhandler} onDoubleClick={DoubleClickhandler}>
+        SHOP NOW
+      </button>
+    </div>
+  );
 }
 
 export default Leftside;
--- REACT/Ecommerse_landing_page/src/components/rightside_div.jsx
+++ REACT/Ecommerse_landing_page/src/components/rightside_div.jsx
@@ -1,10 +1,15 @@
 import "./rightside_div.css"
 import plant from "./plant.jpg";
 
+let mouseoverhandler=()=>{
+    console.log("this is image");
+}
+
 function Rightside(){
+
     return(
         <div className="rightside_div">
-            <img src={plant} alt="" />
+            <img onMouseOver={mouseoverhandler} src={plant} alt="" />
         </div>
     )
 }
