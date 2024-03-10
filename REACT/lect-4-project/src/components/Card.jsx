e00075ab122b57598125526a4daa3f59fc101d32
shreyashjunje <shreyashjunje2003@gmail.com>
2024-03-11 00:03:09 +0530

    .

--- REACT/lect-4-project/src/components/Card.jsx
+++ REACT/lect-4-project/src/components/Card.jsx
@@ -4,7 +4,6 @@ import { toast } from "react-toastify";
 
 export default function Card(props) {
   let course = props.course;
-  
   let likedCourses = props.likedCourses;
 
   let setLikedCourses = props.setLikedCourses;
